a3a2d6ac081fa90b686abd17954ce6b1e3a57a87
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-11 20:25:22 +0530

    Add Pet model

---
+++ src/main/java/models/Pet.java
@@ -0,0 +1,108 @@
+package models;
+
+import java.util.List;
+
+public class Pet {
+    private long id;
+    private String name;
+    private String status;
+    private Category category;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+
+    // Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public static class Category {
+        private long id;
+        private String name;
+
+        // Getters and Setters
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class Tag {
+        private long id;
+        private String name;
+
+        // Getters and Setters
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
