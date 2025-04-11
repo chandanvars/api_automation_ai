d6588606a743a1133cdfa52c2ab881cc14b27aeb
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-11 20:25:36 +0530

    Add TestDataGenerator class

---
+++ src/test/java/utils/TestDataGenerator.java
@@ -0,0 +1,18 @@
+package utils;
+
+import models.Pet;
+
+import java.util.Collections;
+
+public class TestDataGenerator {
+    public static Pet generatePet(long id, String name, String status) {
+        Pet pet = new Pet();
+        pet.setId(id);
+        pet.setName(name);
+        pet.setStatus(status);
+        pet.setCategory(new Pet.Category());
+        pet.setPhotoUrls(Collections.singletonList("https://example.com/photo.jpg"));
+        pet.setTags(Collections.singletonList(new Pet.Tag()));
+        return pet;
+    }
+}
