c0a0fa98ab4513b83ed9e2277e3e04ffe99c578a
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:33:04 +0530

    Update ApiUtils.java file

--- src/test/java/utils/ApiUtils.java
+++ src/test/java/utils/ApiUtils.java
@@ -1,44 +1,49 @@
 package utils;
 
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
 
 public class ApiUtils {
     public static Response post(String endpoint, Object body) {
         return given()
-                .contentType("application/json")
+                .header("Content-Type", "application/json")
                 .body(body)
+                .when()
                 .post(endpoint);
     }
 
     public static Response put(String endpoint, Object body) {
         return given()
-                .contentType("application/json")
+                .header("Content-Type", "application/json")
                 .body(body)
+                .when()
                 .put(endpoint);
     }
 
     public static Response get(String endpoint) {
         return given()
+                .when()
                 .get(endpoint);
     }
 
     public static Response delete(String endpoint) {
         return given()
+                .when()
                 .delete(endpoint);
     }
 
     public static Response postWithFormParams(String endpoint, String key, String value) {
         return given()
-                .contentType("application/x-www-form-urlencoded")
+                .header("Content-Type", "application/x-www-form-urlencoded")
                 .formParam(key, value)
+                .when()
                 .post(endpoint);
     }
 
     public static Response uploadFile(String endpoint, String filePath) {
         return given()
                 .multiPart("file", filePath)
+                .when()
                 .post(endpoint);
     }
 }
