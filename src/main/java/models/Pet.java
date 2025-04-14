bc1a5f10216d69043ace2eacbea170fe1f50bc64
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 22:41:54 +0530

    Create model classes

---
+++ src/main/java/models/Pet.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Pet {
+    // Pet model attributes
+}
+
+public class ResponseModel {
+    // Response model attributes
+}
+
+public class UploadResponse {
+    // Upload response attributes
+}
