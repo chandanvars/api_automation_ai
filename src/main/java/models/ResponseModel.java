03f38f373c33e442c881df7182c6f74aab89f9da
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:32:55 +0530

    Update ResponseModel.java file

--- src/main/java/models/ResponseModel.java
+++ src/main/java/models/ResponseModel.java
@@ -1,12 +1,13 @@
 package models;
 
+import java.util.Objects;
+
 public class ResponseModel {
     private int code;
     private String type;
     private String message;
 
     // Getters and Setters
-
     public int getCode() {
         return code;
     }
@@ -29,5 +30,31 @@ public class ResponseModel {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    // Override equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResponseModel that = (ResponseModel) o;
+        return code == that.code &&
+                Objects.equals(type, that.type) &&
+                Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, type, message);
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "ResponseModel{" +
+                "code=" + code +
+                ", type='" + type + '\'' +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
