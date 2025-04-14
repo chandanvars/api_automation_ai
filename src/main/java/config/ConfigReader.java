87ec2aeb54afde8207fdbaf35d0e70920e0e7acc
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:32:42 +0530

    Update ConfigReader.java file

--- src/main/java/config/ConfigReader.java
+++ src/main/java/config/ConfigReader.java
@@ -1,22 +1,25 @@
 package config;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties properties = new Properties();
 
     static {
-        try {
-            FileInputStream fis = new FileInputStream("config.properties");
-            properties.load(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
     public static String getBaseUrl() {
-        return properties.getProperty("base_url");
+        return properties.getProperty("base.url");
     }
 }
