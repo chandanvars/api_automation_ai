6ee79539ab6655eee14185ce70f41da55cc797ef
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:32:46 +0530

    Update EndpointHelper.java file

--- src/main/java/helpers/EndpointHelper.java
+++ src/main/java/helpers/EndpointHelper.java
@@ -4,12 +4,11 @@ public class EndpointHelper {
     // Pet endpoints
     public static final String ADD_PET = "/pet";
     public static final String UPDATE_PET = "/pet";
-    public static final String GET_PET_BY_ID = "/pet/{petId}";
+    public static final String FIND_PET_BY_ID = "/pet/{petId}";
     public static final String FIND_PETS_BY_STATUS = "/pet/findByStatus";
     public static final String UPDATE_PET_FORM = "/pet/{petId}";
     public static final String DELETE_PET = "/pet/{petId}";
-    public static final String UPLOAD_PET_IMAGE = "/pet/{petId}/uploadImage";
-
+    
     // Store endpoints
     public static final String PLACE_ORDER = "/store/order";
     public static final String GET_ORDER_BY_ID = "/store/order/{orderId}";
