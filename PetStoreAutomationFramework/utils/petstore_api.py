6e7ee8359dc0235df507443999c9287abc9c915e
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 22:50:36 +0530

    Add PetStore API interaction module

---
+++ PetStoreAutomationFramework/utils/petstore_api.py
@@ -0,0 +1,24 @@
+"""
+Module: petstore_api.py
+This module contains functions to interact with the PetStore API.
+"""
+
+class PetStoreAPI:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def add_pet(self, pet_data):
+        # Code to add a new pet to the PetStore
+        pass
+
+    def edit_pet(self, pet_id, pet_data):
+        # Code to edit an existing pet's details
+        pass
+
+    def delete_pet(self, pet_id):
+        # Code to delete a pet from the PetStore
+        pass
+
+    def search_pet(self, search_params):
+        # Code to search for pets in the PetStore
+        pass
