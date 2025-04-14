9f696aaa7013421e20bb9d3aaff1b9600e1b9883
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:32:59 +0530

    Update TestDataGenerator.java file

--- src/test/java/utils/TestDataGenerator.java
+++ src/test/java/utils/TestDataGenerator.java
@@ -15,4 +15,15 @@ public class TestDataGenerator {
         pet.setTags(Collections.singletonList(new Pet.Tag()));
         return pet;
     }
+
+    public static Pet generatePet() {
+        Pet pet = new Pet();
+        pet.setId(System.currentTimeMillis());
+        pet.setName("Dog");
+        pet.setStatus("available");
+        pet.setCategory(new Pet.Category());
+        pet.setPhotoUrls(Collections.singletonList("https://example.com/photo.jpg"));
+        pet.setTags(Collections.singletonList(new Pet.Tag()));
+        return pet;
+    }
 }
