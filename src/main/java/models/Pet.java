66773eb51427ef0aa807be6b6d775f4ff08e73e1
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 22:02:01 +0530

    Add Pet model class

---
+++ src/main/java/models/Pet.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.List;
+
+public class Pet {
+    private long id;
+    private String name;
+    private String status;
+    private List<String> photoUrls;
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+}
