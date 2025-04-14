cca8cda674cc175d6410528f5896673ad29074d3
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-14 21:37:08 +0530

    Create ResponseModel.java for deserializing standard response JSON objects

---
+++ api_automation_ai/src/main/java/models/ResponseModel.java
@@ -0,0 +1,39 @@
+package models;
+
+public class ResponseModel {
+    private int code;
+    private String type;
+    private String message;
+
+    public ResponseModel() {}
+
+    public ResponseModel(int code, String type, String message) {
+        this.code = code;
+        this.type = type;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
