f94bb909bcce7177d7a687b67f76ebaf5401f058
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-16 23:50:21 +0530

    Create EndpointHelper.java

---
+++ src/main/java/helpers/EndpointHelper.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public class EndpointHelper {
+    public static final String PET = "/pet";
+    public static final String PET_BY_ID = "/pet/{petId}";
+    public static final String PET_BY_STATUS = "/pet/findByStatus";
+}
