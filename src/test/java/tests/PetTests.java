4fca2d50e26a5d009580b881e5917bae7a0d2385
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-16 23:36:33 +0530

    Create PetTests with skeleton methods

---
+++ src/test/java/tests/PetTests.java
@@ -0,0 +1,24 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class PetTests extends BaseTest {
+
+    @Test
+    public void testAddNewPet() {
+        // Implementation of test case to add a new pet
+    }
+
+    @Test
+    public void testFindPetById() {
+        // Implementation of test case to find a pet by ID
+    }
+
+    @Test
+    public void testUpdatePet() {
+        // Implementation of test case to update a pet
+    }
+
+    // Additional test methods
+}
