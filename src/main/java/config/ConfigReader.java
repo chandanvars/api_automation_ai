e20de032e964b19076137251dd6c043d734da251
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-16 20:57:53 +0530

    Create ConfigReader.java

---
+++ src/main/java/config/ConfigReader.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            properties.load(input);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getBaseUrl() {
+        return properties.getProperty("baseUrl");
+    }
+}
