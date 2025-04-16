d23514f8a1427f03030a82f6c84b645654c7a115
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-16 23:36:26 +0530

    Create EndpointHelper to manage API endpoints

---
+++ src/main/java/helpers/EndpointHelper.java
@@ -0,0 +1,11 @@
+package helpers;
+
+public class EndpointHelper {
+
+    public static final String ADD_NEW_PET = "/pet";
+    public static final String UPDATE_PET = "/pet";
+    public static final String FIND_PET_BY_ID = "/pet/{petId}";
+    public static final String FIND_PETS_BY_STATUS = "/pet/findByStatus";
+    public static final String UPDATE_PET_WITH_FORM = "/pet/{petId}";
+    public static final String DELETE_PET = "/pet/{petId}";
+}
