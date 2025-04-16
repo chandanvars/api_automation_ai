ccbd10cfb809284add5e5758aa802cf8b6c067c8
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-16 23:50:22 +0530

    Create Pet.java

---
+++ src/main/java/models/Pet.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Pet {
+    private long id;
+    private String name;
+    private String status;
+
+    // Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
