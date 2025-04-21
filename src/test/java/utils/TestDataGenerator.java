4728c5eb3fee6fa9771943612d99f1be70133f2d
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-21 12:32:29 +0530

    Create TestDataGenerator.java for generating test data

--- src/test/java/utils/TestDataGenerator.java
+++ src/test/java/utils/TestDataGenerator.java
@@ -2,6 +2,9 @@ package utils;
 
 import com.github.javafaker.Faker;
 import models.Pet;
+import models.User;
+
+import java.util.UUID;
 
 public class TestDataGenerator {
     private static final Faker faker = new Faker();
@@ -12,5 +15,18 @@ public class TestDataGenerator {
         pet.setName(faker.name().firstName());
         pet.setStatus("available");
         return pet;
+    }
+
+    public static User generateUser() {
+        User user = new User();
+        user.setId(UUID.randomUUID());
+        user.setUsername(faker.name().username());
+        user.setEmail(faker.internet().emailAddress());
+        user.setFirstName(faker.name().firstName());
+        user.setLastName(faker.name().lastName());
+        user.setPassword(faker.internet().password());
+        user.setPhone(faker.phoneNumber().phoneNumber());
+        user.setUserStatus(1); // Assuming 1 is the default status
+        return user;
     }
 }
