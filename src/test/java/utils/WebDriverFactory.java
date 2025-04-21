a2aa9abae9f31b72a8b6ba4ba3446bc2609381ad
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-21 13:07:47 +0530

    Create WebDriverFactory.java for managing WebDriver instances

---
+++ src/test/java/utils/WebDriverFactory.java
@@ -0,0 +1,24 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public static WebDriver createDriver(String browser) {
+        WebDriver driver;
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+        return driver;
+    }
+}
