a27a0bfcc8bd531c24040deefbbaf2fa32d672ea
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-21 13:44:21 +0530

    Create BaseTest.java for WebDriver setup and teardown

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.WebDriverFactory;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.createDriver();
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getProperty("baseUrl"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
