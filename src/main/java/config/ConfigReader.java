18b7ad1099b251b0cda2144b24e2eba221638f6f
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:15:53 +0530

    Create ConfigReader.java

---
+++ src/main/java/config/ConfigReader.java
@@ -0,0 +1,23 @@
+package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private Properties properties;
+
+    public ConfigReader() {
+        properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBaseUrl() {
+        return properties.getProperty("base.url");
+    }
+}
