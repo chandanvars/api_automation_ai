b6f8649e9fccd6b1a63a87a26a9a491b52f86c11
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 22:49:15 +0530

    Create ConfigReader.java to load runtime configurations from config.properties

---
+++ src/main/java/config/ConfigReader.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load config.properties file.");
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static int getIntProperty(String key) {
+        return Integer.parseInt(properties.getProperty(key));
+    }
+
+    public static boolean getBooleanProperty(String key) {
+        return Boolean.parseBoolean(properties.getProperty(key));
+    }
+}
