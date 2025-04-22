86d2928441d21aef55e62d9944e5229f56dd4d98
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:07:43 +0530

    Fix TestDataGenerator.java for valid Pet data generation

--- src/test/java/utils/TestDataGenerator.java
+++ src/test/java/utils/TestDataGenerator.java
@@ -4,13 +4,24 @@ import com.github.javafaker.Faker;
 import models.Pet;
 
 public class TestDataGenerator {
-    private static Faker faker = new Faker();
+    private static final Faker faker = new Faker();
 
     public static Pet generatePet() {
         Pet pet = new Pet();
         pet.setId(faker.number().randomNumber());
-        pet.setName(faker.name().firstName());
+        pet.setName(faker.animal().name());
         pet.setStatus("available");
+        return pet;
+    }
+
+    public static Pet createRandomPet() {
+        Pet pet = new Pet();
+        pet.setId(faker.number().randomNumber());
+        pet.setName(faker.animal().name());
+        pet.setStatus("available");
+        // Assuming Pet has type and age fields, add them if required
+        // pet.setType(faker.animal().type());
+        // pet.setAge(faker.number().numberBetween(1, 15));
         return pet;
     }
 }
