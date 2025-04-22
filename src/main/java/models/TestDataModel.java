0fbc0c0953e68d079d776d43d9159824bb2c68c5
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 22:49:41 +0530

    Create TestDataModel.java to represent test data model for TextBox inputs

---
+++ src/main/java/models/TestDataModel.java
@@ -0,0 +1,50 @@
+package models;
+
+public class TestDataModel {
+    private String fullName;
+    private String email;
+    private String currentAddress;
+    private String permanentAddress;
+
+    public TestDataModel() {
+    }
+
+    public TestDataModel(String fullName, String email, String currentAddress, String permanentAddress) {
+        this.fullName = fullName;
+        this.email = email;
+        this.currentAddress = currentAddress;
+        this.permanentAddress = permanentAddress;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurrentAddress() {
+        return currentAddress;
+    }
+
+    public void setCurrentAddress(String currentAddress) {
+        this.currentAddress = currentAddress;
+    }
+
+    public String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(String permanentAddress) {
+        this.permanentAddress = permanentAddress;
+    }
+}
