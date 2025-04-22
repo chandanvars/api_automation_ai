2cd92435db6d990ac6764667904c593f9925adc5
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:07:37 +0530

    Update Pet.java model class

--- src/main/java/models/Pet.java
+++ src/main/java/models/Pet.java
@@ -1,8 +1,13 @@
 package models;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Pet {
     private Long id;
+    private Category category;
     private String name;
+    private List<Tag> tags;
     private String status;
 
     // Getters and Setters
@@ -14,6 +19,14 @@ public class Pet {
         this.id = id;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
     public String getName() {
         return name;
     }
@@ -22,11 +35,123 @@ public class Pet {
         this.name = name;
     }
 
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    // Nested Category class
+    public static class Category {
+        private Long id;
+        private String name;
+
+        public Category() {
+        }
+
+        public Category(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Category)) return false;
+            Category category = (Category) o;
+            return Objects.equals(id, category.id) &&
+                   Objects.equals(name, category.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "Category{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    // Nested Tag class
+    public static class Tag {
+        private Long id;
+        private String name;
+
+        public Tag() {
+        }
+
+        public Tag(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Tag)) return false;
+            Tag tag = (Tag) o;
+            return Objects.equals(id, tag.id) &&
+                   Objects.equals(name, tag.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "Tag{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 }
