2672345312f6f7bb29c03dd2305f0365aed85640
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:16:03 +0530

    Create TestDataGenerator.java

---
+++ src/test/java/utils/TestDataGenerator.java
@@ -0,0 +1,13 @@
+package utils;
+
+import models.Pet;
+
+public class TestDataGenerator {
+    public static Pet createPet() {
+        Pet pet = new Pet();
+        pet.setId(1);
+        pet.setName("Doggie");
+        pet.setStatus("available");
+        return pet;
+    }
+}
