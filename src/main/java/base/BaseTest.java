f6a42bd825efb1bb8f33498558cf26f3b079f05c
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:07:22 +0530

    Update BaseTest.java to use ConfigReader for base URI

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,10 +3,12 @@ package base;
 import org.testng.annotations.BeforeClass;
 import io.restassured.RestAssured;
 import config.ConfigReader;
+import utils.TestUtils;
 
 public class BaseTest {
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = ConfigReader.getBaseUrl();
+        TestUtils.initialize();
     }
 }
