1c15df9219ed704a323d8287dc4b8747c3558f98
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:21:45 +0530

    Create ResponseModel.java

---
+++ src/main/java/models/ResponseModel.java
@@ -0,0 +1,14 @@
+package models;
+
+public class ResponseModel {
+    private String message;
+
+    // Getters and Setters
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
