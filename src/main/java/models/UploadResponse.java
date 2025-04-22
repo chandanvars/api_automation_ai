4892fb377801f5dc3dd2c0446ddb3360092af832
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-22 23:21:46 +0530

    Create UploadResponse.java

---
+++ src/main/java/models/UploadResponse.java
@@ -0,0 +1,14 @@
+package models;
+
+public class UploadResponse {
+    private String message;
+
+    // Getters and Setters
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
