e3821d585e455a5d35ca78c6b584ae304dd2810e
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-23 23:49:59 +0530

    Update WebDriverFactory.java for WebDriver instance creation with logging

--- src/test/java/utils/WebDriverFactory.java
+++ src/test/java/utils/WebDriverFactory.java
@@ -7,11 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class WebDriverFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
+
     public static WebDriver createDriver() {
         String browser = ConfigReader.getProperty("browser").toLowerCase();
+        WebDriver driver;
         switch (browser) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
@@ -19,16 +26,29 @@ public class WebDriverFactory {
                 if (ConfigReader.getBooleanProperty("headless")) {
                     chromeOptions.addArguments("--headless=new");
                 }
-                return new ChromeDriver(chromeOptions);
+                driver = new ChromeDriver(chromeOptions);
+                break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 if (ConfigReader.getBooleanProperty("headless")) {
                     firefoxOptions.addArguments("-headless");
                 }
-                return new FirefoxDriver(firefoxOptions);
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
             default:
-                throw new RuntimeException("Unsupported browser: " + browser);
+                logger.warn("Unsupported browser '{}', falling back to Chrome.", browser);
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions defaultOptions = new ChromeOptions();
+                if (ConfigReader.getBooleanProperty("headless")) {
+                    defaultOptions.addArguments("--headless=new");
+                }
+                driver = new ChromeDriver(defaultOptions);
+                break;
         }
+        driver.manage().window().maximize();
+        int implicitWaitSeconds = ConfigReader.getIntProperty("implicitWait", 10);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitSeconds));
+        return driver;
     }
 }
