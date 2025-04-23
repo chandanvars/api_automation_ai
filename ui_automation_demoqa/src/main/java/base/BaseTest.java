d28313c514f9c390cdaf6e94519a47a20f886d98
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-24 00:09:22 +0530

    Create BaseTest.java

---
+++ ui_automation_demoqa/src/main/java/base/BaseTest.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.WebDriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.createWebDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
